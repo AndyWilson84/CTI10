fba5c06c69cffddc59224b0f2cba8aaf1d9c01bf
AndyWilson84 <160041580+AndyWilson84@users.noreply.github.com>
2024-02-14 10:33:48 -0500

    Add files via upload

---
+++ P1LAB1_WilsonAndy.py
@@ -0,0 +1,14 @@
+#Andy Wilson
+#2/12/2024
+#P1LAB1
+#Using input and output
+
+#Create a variable to hold first name
+first_name = input ("Enter your first name: ")
+
+#Print first_name with greeting
+print ("Hey", first_name)
+print ()
+print ("Welcome to zyBooks!")
+
+#For break it's print() inbetween as above or print ("Hey", first_name, "\nWelcome to zyBooks!"
